fa2e25ba7db96dc757c4aec6f1e59dc5a46e0f31
genpeng <617735343@qq.com>
2019-02-22 00:01:32 +0800

    旅游网

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 // 路由就是根据网址的不同，返回的内容就不同
 /* eslint-disable no-new */
