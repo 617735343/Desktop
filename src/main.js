467082a1a647cf929956c3aaf8e645517471283f
genpeng <617735343@qq.com>
2019-02-21 21:10:52 +0800

    旅游网

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
+// 路由就是根据网址的不同，返回的内容就不同
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
