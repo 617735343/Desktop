eb3f1d3d80ca9829d4a0d226aee5eb0f024c42dc
genpeng <617735343@qq.com>
2019-02-22 23:11:34 +0800

    旅游网

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,63 +1,73 @@
 <template>
-    <div class="header">
-        <div class="header-left">
-            <div class="iconfont back-icon">&#xe624;</div>
-        </div>
-        <div class="header-input">
-            <span class="iconfont">&#xe632;</span>
-            输入城市/景点/游玩主题
-        </div>
-        <div class="header-right">
-            {{this.city}}
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
     </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"HomeHeader",
-    props:{
-        city:String
-    }
-}
+  name: "HomeHeader",
+  props: {
+    city: String
+  }
+};
 </script>
 
 
 <style lang="stylus" scoped>
-    @import '../../../assets/styles/varibles'
-    .header
-        display: flex
-        height: .86rem
-        line-height: .86rem
-        background: $bgColor
-        color: #fff
-        
-        .header-left
-            width: .64rem
-            float: left
+@import '../../../assets/styles/varibles';
 
-            .back-icon
-                text-align:center
-                font-size:.4rem
+.header {
+  display: flex;
+  height: $headerHeight;
+  line-height: $headerHeight;
+  background: $bgColor;
+  color: #fff;
 
-        .header-input
-            flex: 1
-            height:.64rem
-            margin-top: .12rem
-            margin-left: .2rem
-            padding-left: .2rem
-            line-height: .64rem
-            background:#fff
-            border-radius: .1rem
-            color:#ccc
-        .header-right
-            width: 1.24rem;
-            float: right;
-            text-align: center
+  .header-left {
+    width: 0.64rem;
+    float: left;
 
-            .arrow-icon
-                font-size .24rem
-                margin-left -.04rem
-            
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    line-height: 0.64rem;
+    background: #fff;
+    border-radius: 0.1rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
+    color: #fff;
+
+    .arrow-icon {
+      font-size: 0.24rem;
+      margin-left: -0.04rem;
+    }
+  }
+}
 </style>
