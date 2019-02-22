0e0745c33c601597ad2d1cfbf215725088469839
genpeng <617735343@qq.com>
2019-02-22 23:59:07 +0800

    旅游网

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音">
-    </div>
-
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  </div>
 </template>
 
 <script>
@@ -13,18 +12,21 @@ export default {
 
 <style lang='stylus' scoped>
 @import '../../../assets/styles/varibles.styl';
-    .search
-        height :.72rem
-        padding :0 .1rem
-        background :$bgColor
-        .search-input
-            width :100%
-            height :.62rem
-            box-sizing :border-box
-            padding :0 .1rem
-            line-height :.62rem
-            text-align :center
-            border-radius :.06rem
-            color :#666
 
+.search {
+  height: 0.72rem;
+  padding: 0 0.1rem;
+  background: $bgColor;
+
+  .search-input {
+    width: 100%;
+    height: 0.62rem;
+    box-sizing: border-box;
+    padding: 0 0.1rem;
+    line-height: 0.62rem;
+    text-align: center;
+    border-radius: 0.06rem;
+    color: #666;
+  }
+}
 </style>
