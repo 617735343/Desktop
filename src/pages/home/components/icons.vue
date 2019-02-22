04084f88fe5cab1c2c1c7013e099f0aed7f96a23
genpeng <617735343@qq.com>
2019-02-22 19:43:03 +0800

    旅游网

---
+++ src/pages/home/components/icons.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="icons">
+    <swiper ref="mySwiper">
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div 
+            class="icon" 
+            v-for="item of page"
+            :key="item.id"
+            >
+          <div class="icon-img">
+            <img
+              class="icon-img-content"
+              :src="item.imgUrl"
+            >
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeIcons",
+  data(){
+      return {
+          iconList:[{
+              id:'001',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+              desc:'景点门票'
+          },{
+              id:'002',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+              desc:'广州必游'
+          },{
+              id:'003',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
+              desc:'动植物园'
+          },{
+              id:'004',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png",
+              desc:'游乐场'
+          },{
+              id:'005',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+              desc:'景点门票'
+          },{
+              id:'006',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+              desc:'广州必游'
+          },{
+              id:'007',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
+              desc:'动植物园'
+          },{
+              id:'008',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png",
+              desc:'游乐场'
+          },{
+              id:'009',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png",
+              desc:'玩转长隆'
+          }]
+      }
+  },
+  computed:{
+      pages (){
+          const pages = [];
+          this.iconList.forEach((item,index) => {
+              const page = Math.floor(index / 8);
+              if(!pages[page]){
+                  pages[page] = [];
+              }
+              pages[page].push(item)
+          })
+          return pages
+      }
+  }
+};
+</script>
+
+<style lang='stylus' scoped>
+@import '../../../assets/styles/varibles';
+@import '../../../assets/styles/mixins';
+
+.icons >>> .swiper-container {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  position: relative;
+  overflow: hidden;
+  float: left;
+  width: 25%;
+  height: 0;
+  padding-bottom: 25%;
+
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+    padding: 0.1rem;
+
+    .icon-img-content {
+      display: block;
+      margin: 0 auto;
+      height: 100%;
+    }
+  }
+
+  .icon-desc {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    color: $darkTextColor;
+    text-align: center;
+    ellipsis()
+  }
+}
+</style>
