b5840769533bb2e25a102b0799a2b6d69d7f4aa3
genpeng <617735343@qq.com>
2019-02-24 23:25:25 +0800

    旅游网

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <router-link tag="div" to="/" class="header-abs"
-    v-show="showAbs">
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
       <div class="iconfont header-abs-back">&#xe624;</div>
     </router-link>
     <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
@@ -15,44 +14,45 @@
 <script>
 export default {
   name: "DetailHeader",
-  data () {
-      return {
-          showAbs: true,
-          opacityStyle: {
-              opacity: 0
-          }
+  data() {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
       }
+    };
   },
   methods: {
-      handleScroll() {
-          const top = document.documentElement.scrollTop;
-          if(top > 60) {
-              let opacity = top / 140;
-              opacity = opacity > 1 ? 1 : opacity;
-              this.opacityStyle = {
-                  opacity:opacity
-              }
-              this.showAbs = false;
-          }else{
-              this.showAbs = true;
-          }
+    handleScroll() {
+      const top = document.documentElement.scrollTop;
+      if (top > 60) {
+        let opacity = top / 140;
+        opacity = opacity > 1 ? 1 : opacity;
+        this.opacityStyle = {
+          opacity: opacity
+        };
+        this.showAbs = false;
+      } else {
+        this.showAbs = true;
       }
+    }
   },
   //每次页面展示的时候被执行
   activated() {
-      //绑定scroll事件
-      window.addEventListener('scroll',this.handleScroll);
+    //绑定scroll事件
+    window.addEventListener("scroll", this.handleScroll);
   },
   //页面即将被隐藏，或者被替换成新的页面的时候执行
   deactivated() {
-      //解绑scroll事件
-      window.removeEventListener('scroll',this.handleScroll);
+    //解绑scroll事件
+    window.removeEventListener("scroll", this.handleScroll);
   }
 };
 </script>
 
 <style lang="stylus" scoped>
 @import '../../../assets/styles/varibles.styl';
+
 .header-abs {
   position: absolute;
   left: 0.2rem;
@@ -71,10 +71,11 @@ export default {
 }
 
 .header-fixed {
+  z-index: 2;
   position: fixed;
-  top :0
-  left :0
-  right :0
+  top: 0;
+  left: 0;
+  right: 0;
   height: $headerHeight;
   line-height: $headerHeight;
   text-align: center;
